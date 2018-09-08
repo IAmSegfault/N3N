c9d8d7df06b8b200f9719bc59d57c2d4591477c8
Joe <IAmSegfault@users.noreply.github.com>
2018-09-08 16:49:28 -0700

    Added constraints to make the function truly bijective
    Cannot exceed 131072 chunks.

--- Bijection.lua
+++ Bijection.lua
@@ -1,19 +1,24 @@
 
 -- Useful for example, when creating a deterministic seed for a given chunk coordinate.
--- (x,y,z)->(x/1)+((x+y+1)/2)+((x+y+z+2)/3).
+-- (x,y,z)->(x combine 1)+((x+y+1) combine 2)+((x+y+z+2) combine 3).
 -- See https://math.stackexchange.com/questions/1176184/how-to-find-unique-numbers-from-3-numbers
 -- https://math.stackexchange.com/questions/312879/how-to-define-a-injective-and-surjective-function-from-mathbbz-to-mathbb
 -- https://en.wikipedia.org/wiki/Combination#Example_of_counting_combinations 
 -- for details.
 local bijection = {}
-local magic = 8675309;
+local maxint = 131072;
+
+-- abs(magic) must not exceed 37483350946
+local magic = 9001;
 
 local function combine(n, k)
-  if n >0 then
-    n = 2 * n
-  else
+  if n >0 and n <= maxint then
+    n = 2 * n 
+  elseif n > (maxint * -1)
     n = (-2 * n) + 1
   end
+  
+  
   local c = 1;
   for i = 1, k do
     x = n - (i - 1);
@@ -23,6 +28,8 @@ local function combine(n, k)
 end
 
 function bijection.map(x,y,z)
+  
+  
   z = z or 0;
   local n1 = x;
   local x1 = combine(n1, 1);
@@ -32,6 +39,10 @@ function bijection.map(x,y,z)
 
   local n3 = x + y + z + 2;
   local x3 = combine(n3, 3);
+  
+  if not x1 or not x2 or not x3 then 
+    return nil
+  end
   
   local r = (x1 + x2 + x3) * magic
   --Faster rounding than floor.
